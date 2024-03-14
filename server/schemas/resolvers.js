0f1f737555d10237d8d9116b2001649573629ae7
Kevin Hogan <kchogan@pacbell.net>
2024-03-13 17:58:30 -0700

    moving key to env

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,8 @@
 const { User, Product, Category, Order } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const resolvers = {
   Query: {
